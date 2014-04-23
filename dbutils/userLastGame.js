2107f6d52025bb7068174bf970c9d655c2bb3dd5
baffonero <fabiodellerba@gmail.com>
2014-04-23 10:59:25 +0200

    userLastGame

--- dbutils/userLastGame.js
+++ dbutils/userLastGame.js
@@ -16,10 +16,8 @@ var curr = mydb.users.find({"gamesdet.scopa":{$exists:true}, "gamesdet.scopa.lg"
 
 curr.forEach( function(user) {
     tot += 1;
-	//print("NamePre:"+vName);
     var usrObj = {guid:user.guid};  
     getLastGame(usrObj, function (lastGame)  {
-		print("Name:"+vName+" Nick:"+defNick);
 		if (lastgame) {
 		  //user.gamesdet.scopa.lg = lastGame;
 		  //mydb.users.save(user);	
